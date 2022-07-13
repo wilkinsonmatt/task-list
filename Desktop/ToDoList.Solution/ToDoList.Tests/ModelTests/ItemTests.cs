0dd58313d107009ab65fbad2f8f4fc17971059d2
Claire Thorington <claire.thorington@gmail.com>
2022-07-13 11:01:05 -0700

    create initial files
    Co-authored-by: matt wilkinson <wilkinsonmatt01@gmail.com>

---
+++ Desktop/ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class ItemTests : IDisposable
+    {
+      public void Dispose()
+      {
+        Item.ClearAll();
+      }
+
+    [TestMethod]
+      public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test"); //We pass in "test" as an argument here.
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
+  }
+}
